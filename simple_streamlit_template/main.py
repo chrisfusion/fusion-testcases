377d15b90cc7a7fd294f5bd0d143aa140717f63c
chrisfusion <christianpeh966@gmail.com>
2026-05-31 23:20:28 +0200

    bad bugs

--- simple_streamlit_template/main.py
+++ simple_streamlit_template/main.py
@@ -3,9 +3,10 @@ import sys
 
 
 def main() -> None:
+    import internals
     import streamlit.web.cli as stcli
 
-    app_path = os.path.join(os.path.dirname(__file__), "internals", "app.py")
+    app_path = os.path.join(os.path.dirname(internals.__file__), "app.py")
     sys.argv = ["streamlit", "run", app_path] + sys.argv[1:]
     stcli.main()
 
